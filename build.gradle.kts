88ef494d1489f24432996d0caaa065d1d32b9c92
Guido Zuccarelli <guidozuccarelli@hotmail.com>
2025-05-12 19:22:49 +1000

    Add spotless and apply (#9)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    id("com.diffplug.spotless") version "7.0.3"
 }
 
 group = "dev.zucca-ops"
@@ -10,10 +11,21 @@ repositories {
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.9.1"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+
 }
 
-tasks.test {
-    useJUnitPlatform()
+subprojects {
+    apply(plugin = "com.diffplug.spotless")
+
+    configure<com.diffplug.gradle.spotless.SpotlessExtension> {
+        java {
+            target("src/main/java/**/*.java")
+            licenseHeaderFile(rootProject.file("config/license-header.txt"))
+        }
+
+        format("testJava") {
+            target("src/test/java/**/*.java")
+            java{ }
+        }
+    }
 }
