aa8d63e740f9b2776bdb0edb2d62e39d4730927e
Guido Zuccarelli <guidozuccarelli@hotmail.com>
2026-03-28 14:33:32 +1100

    upgrade assertj version

--- functional-test/build.gradle.kts
+++ functional-test/build.gradle.kts
@@ -18,7 +18,7 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testImplementation("ch.qos.logback:logback-classic:1.5.32")
-    testImplementation("org.assertj:assertj-core:3.26.0")
+    testImplementation("org.assertj:assertj-core:3.27.7")
     testImplementation("info.picocli:picocli:4.7.7")
     testImplementation("org.yaml:snakeyaml:2.2")
 }
